89ce99d688b105018b6c385f8f9c9bba4f3eb580
Allysom Oliveira <allysomoliveira7@gmail.com>
2020-06-15 17:01:33 -0300

    Criado console via dotnet para iniciar ambiente de teste.

---
+++ dotnet-teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
